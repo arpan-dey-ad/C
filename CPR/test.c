6617ff5461517009afa972d92b52fd919a3cb5d1
arpan-dey-ad <arpan.dey.code@gmail.com>
2023-02-02 16:57:39 +0530

    init

--- CPR/test.c
+++ CPR/test.c
@@ -1,1 +1,13 @@
-test
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter a number: ");
+    scanf("%d",&n);
+    {
+        while (i<=5)
+            printf("hello")
+            n++;
+            //test
+    }
+}
---
+++ DS/Pointers/dec.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+{
+    int i = 50;
+    int *ii;
+    ii = &i;
+    printf("The address of i = %u\n", ii);
+    
+}
