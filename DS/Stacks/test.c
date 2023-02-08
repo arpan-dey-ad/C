a60647f0853c72af24d30f280a8d324298ef5cba
arpan-dey-ad <arpan.dey.code@gmail.com>
2023-02-08 10:42:56 +0530

    init

---
+++ DS/Stacks/test.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#define Max 50
+int stack[Max],top=-1,n=Max-1,i;
+void pop();
+void push();
+void show();
+
+int main() {
+    push();
+    push();
+    push();
+    push();
+    push();
+    push();
+    show();
+    pop();
+    show();
+    pop();
+    show();
+    push();
+    show();
+
+    return 0;
+}
+
+void push ()  
+{  
+    int val;      
+    if (top == n )   
+    printf("\n Overflow");   
+    else   
+    {  
+        printf("\nEnter the value : ");  
+        scanf("%d",&val);         
+        top = top +1;   
+        stack[top] = val;   
+    }   
+}  
+void pop ()   
+{   
+    printf("\nPOP");
+    if(top == -1)   
+    printf("Underflow");  
+    else  
+    top = top -1;   
+}   
+void show()  
+{  
+    printf("\nValues in stack are : ");
+    for (i=top;i>=0;i--)  
+    {  
+        printf("\n%d",stack[i]);  
+    }  
+    if(top == -1)   
+    {  
+        printf("Stack is empty\n");  
+    }  
+}  
