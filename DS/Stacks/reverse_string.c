d47168d624e85815cce5e16a712ed1454ebc3aed
arpan-dey-ad <arpan.dey.code@gmail.com>
2023-02-09 11:00:03 +0530

    reverse of a string

--- DS/Stacks/reverse_string.c
+++ DS/Stacks/reverse_string.c
@@ -3,16 +3,16 @@
 #include <string.h>  
 void main()  
 {  
-    char str[50], temp; // define the size of str[] array  
+    char str[50], temp;  
     int i, left, right, len;  
     printf (" \n Display a reverse string in the C: \n");  
     printf (" ----------------------- ");  
     printf (" \n Enter a string to reverse order: ");  
     scanf( "%s", &str);  
-    len = strlen(str); // get the length of the string  
-    left = 0; // set left index at 0  
-    right = len - 1; // set right index len - 1  
-    // use for loop to store the reverse string  
+    len = strlen(str);  
+    left = 0;  
+    right = len - 1;  
+     
     for (i = left; i <right; i++)  
     {  
         temp = str[i];  
