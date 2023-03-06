8905bbb44dcec062c9de5f637c9051e781b2eeaa
Arpan <arpan.dey.code@gmail.com>
2023-03-06 10:58:22 +0530

    init

--- CPR/String/1.c
+++ CPR/String/1.c
@@ -2,12 +2,12 @@
 #include<string.h>
 void main()
 {
-    char arr1[10], arr2[10], arr3[10];
+    char arr1[20], arr2[20];
     printf("Enter your Name: ");
     scanf("%s",&arr1);
     printf("Enter your Surname: ");
     scanf("%s",&arr2);
     strcat(arr1,arr2);
-    printf("Your Name is : %s",arr3);
+    printf("Your Name is : %s", arr1);
 
 }
